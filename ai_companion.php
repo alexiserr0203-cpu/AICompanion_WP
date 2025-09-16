1007d7d1999ed188ee75e44c99e4d9919e1d2401
alexiserr0203-cpu <alexiserr0203@gmail.com>
2025-09-16 10:22:28 -0400

    final update

--- ai_companion.php
+++ ai_companion.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * Plugin Name:       AICompanion_WP
- * Description:       Your Virtual AI assistant to make your WordPress content automation journey smooth and beautiful using Open AI.
- * Requires at least: 5.8
- * Requires PHP:      7.4
- * Version:           1.2.0
- * Author:            Maniruzzaman Akash<manirujjamanakash@gmail.com>
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       innovator-ai
- */
-
 defined( 'ABSPATH' ) || exit;
 
 /**
